b48b79e8786e638a131e48bd08201c7f066dc66a
Guilherme Fusuma <fuskenji@gmail.com>
2025-03-25 13:56:43 -0300

    modificação do mapa

--- camera_test.py
+++ camera_test.py
@@ -1,9 +1,11 @@
 import pygame
 import sys
+import random
 
 from character import Player_rect
 from guns import Gun, Laser_gun, EQUIP_EVENT, DEQUIP_EVENT
-from camera import create_screen, camera
+from camera import camera
+from game_map import *
 
 pygame.init()
 
@@ -11,42 +13,15 @@ pygame.init()
 clock = pygame.time.Clock()
 FPS = 60
 
-TILES_SIZE = 50
-
-SCREEN_WIDTH = TILES_SIZE * 20
-SCREEN_HEIGHT = TILES_SIZE * 16
-
 plr_col = (255, 0, 0)
 
-
-
-tile_map = [
-    [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2],
-    [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2],
-    [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [0, 0, 0, 3, 3, 0, 0, 0, 0, 1, 1, 1, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [1, 1, 1, 3, 3, 1, 1, 1, 0, 1, 1, 1, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [1, 1, 1, 3, 3, 1, 1, 1, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 2, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2],
-    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2],
-    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [0, 0, 0, 3, 3, 1, 1, 1, 1, 0, 0, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [1, 1, 1, 3, 3, 1, 1, 1, 0, 0, 0, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [0, 0, 0, 3, 3, 0, 0, 0, 1, 1, 1, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [1, 1, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [1, 1, 1, 3, 3, 0, 0, 0, 1, 0, 0, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 1]
+tile_kinds = [
+    Tile_kind("sand", "Img/tiles/areia.png", False),
+    Tile_kind("sand", "Img/tiles/areia_2.png", False),
+    Tile_kind("sand", "Img/tiles/areia_3.png", False),
 ]
 
-#---------------------instances--------------------------------------
-screen = create_screen(SCREEN_WIDTH, SCREEN_HEIGHT, 'Scrolling test')
+map = Map("map/mapa_1.csv", tile_kinds, TILES_SIZE)
 
 player = Player_rect(300, 200, 20, plr_col)
 
@@ -59,21 +34,8 @@ obj_surface = pygame.surface.Surface((70, 70))
 obj = pygame.rect.Rect(600 - camera.x, 600 - camera.y, obj_surface.get_width(), obj_surface.get_height())
 
 def update_screen(player, gun, camera):
-    camera.x = max(0, min(player.rect.x - SCREEN_WIDTH // 2, (len(tile_map[0]) * TILES_SIZE) - SCREEN_WIDTH))
-    camera.y = max(0, min(player.rect.y - SCREEN_HEIGHT // 2, (len(tile_map) * TILES_SIZE) - SCREEN_HEIGHT))
-
-    for row in range(len(tile_map)):
-        for col, tile in enumerate(tile_map[row]):
-            x = col * TILES_SIZE - camera.x
-            y = row * TILES_SIZE - camera.y
-
-            if -TILES_SIZE <= x < SCREEN_WIDTH and -TILES_SIZE <= y < SCREEN_HEIGHT:
-                if tile == 1: 
-                    pygame.draw.rect(screen, (139, 180, 59), (x, y, TILES_SIZE, TILES_SIZE))  # Cor marrom
-                elif tile == 2: 
-                    pygame.draw.rect(screen, (0, 0, 255), (x, y, TILES_SIZE, TILES_SIZE))  # Cor azul
-                else:  
-                    pygame.draw.rect(screen, (50, 120, 80), (x, y, TILES_SIZE, TILES_SIZE))
+    camera.x = max(0, min(player.rect.x - SCREEN_WIDTH // 2, (len(map.tiles[0]) * TILES_SIZE) - SCREEN_WIDTH))
+    camera.y = max(0, min(player.rect.y - SCREEN_HEIGHT // 2, (len(map.tiles) * TILES_SIZE) - SCREEN_HEIGHT))
 
     player.update(screen)
 
@@ -89,7 +51,7 @@ def game():
     while run:
         clock.tick(FPS)
 
-        screen.fill((0, 0, 0))
+        map.draw(screen)
 
         update_screen(player, gun, camera)
 
---
+++ game_map.py
@@ -0,0 +1,42 @@
+import pygame
+from camera import create_screen, camera
+import csv
+
+TILES_SIZE = 32
+
+SCREEN_WIDTH = TILES_SIZE * 20
+SCREEN_HEIGHT = TILES_SIZE * 16
+
+screen = create_screen(SCREEN_WIDTH, SCREEN_HEIGHT, 'Scrolling test')
+
+class Tile_kind:
+    def __init__(self, name, image, is_solid):
+        self.name = name
+        self.image = pygame.image.load(image)
+        self.is_solid = is_solid
+
+class Map:
+    def __init__(self, map_file, tile_kinds, tile_size):
+        self.tile_kinds = tile_kinds
+        self.tiles = []
+        self.tile_size = tile_size
+
+        # usa o arquivo csv para pegar o mapa
+        with open(map_file, mode='r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                new_row = []    
+                for item in row:
+                    int_item = int(item)
+                    new_row.append(int_item)
+                self.tiles.append(new_row)
+
+
+    def draw(self, screen):
+        for y, row in enumerate(self.tiles):
+            for x, tile in enumerate(row):
+                location = (x * self.tile_size - camera.x, y * self.tile_size - camera.y)
+                image = self.tile_kinds[tile].image
+                screen.blit(image, location)
+        
+        
